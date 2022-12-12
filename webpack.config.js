6f26205be689528bef16e137f8ded8538b658ac8
ChuckleBuckle <102398736+ChuckleBuckle11@users.noreply.github.com>
2022-12-12 14:56:43 +0800

    Initial Commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,10 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
-  entry: {
-    index: './src/index.js'
-  },
-  devtool: 'inline-source-map',
-  devServer: {
-    static: './dist',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Restaurant Page',
-      template: "./src/template.html",
-    }),
-  ],
+  mode:'development',
+  entry: './src/index.js',
   output: {
-    filename: '[name].bundle.js',
+    filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true,
-  },
-  optimization: {
-    runtimeChunk: 'single',
   },
 };
